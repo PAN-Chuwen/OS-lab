8c1c488ba94c15c2a09fb07638fac17251dbb870
Pancw2000 <panchuwen2000@gmail.com>
2022-11-11 15:57:44 +0800

    puts() puti() in sbi.c finishied

--- src/lab1/lib/print.c
+++ src/lab1/lib/print.c
@@ -2,9 +2,38 @@
 #include "sbi.h"
 
 void puts(char *s) {
-    // unimplemented
+    for (; *s != '\0'; s++) {
+        sbi_ecall(0x01, 0x0, *s, 0, 0, 0, 0, 0);
+    }
 }
 
 void puti(int x) {
-    // unimplemented
+    const int base = 10;
+    int power = 1;
+    // avoid using array 
+    do
+    {
+        power *= 10;
+    } while (x >= power);
+    /*
+        x = 10 -> power = 100
+    */
+    
+    /*
+        power /= 10 -> power = 10
+        curDigit = '1'
+        x = 10 % 10 = 0
+        ...
+    */
+
+    while (power != 1)
+    {
+        power /= base;
+        char curDigitASCII = (x / power) + '0';
+        sbi_ecall(0x1, 0x0, curDigitASCII, 0, 0, 0, 0, 0);
+        x = x % power;
+    }
+    
+
+    sbi_ecall(0x1, 0x0, x, 0, 0, 0, 0, 0);
 }
