5756b01a428b2bc811091e8385723722360b40f0
Pancw2000 <panchuwen2000@gmail.com>
2022-11-11 14:48:35 +0800

    sbi_ecall() prints '3'

--- src/lab1/init/main.c
+++ src/lab1/init/main.c
@@ -4,9 +4,9 @@
 extern void test();
 
 int start_kernel() {
+    sbi_ecall(0x1, 0x0, 0x33, 0, 0, 0, 0, 0); 
     puti(2022);
     puts(" Hello RISC-V\n");
-
     test(); // DO NOT DELETE !!!
 
 	return 0;
