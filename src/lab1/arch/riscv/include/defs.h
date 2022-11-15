d4cdbe220bdf0acc9dba634574563e51929efae6
Pancw2000 <panchuwen2000@gmail.com>
2022-11-15 16:50:16 +0800

    macro csr_read completed, add tests for read/write csr macro in test.c

--- src/lab1/arch/riscv/include/defs.h
+++ src/lab1/arch/riscv/include/defs.h
@@ -2,14 +2,23 @@
 #define _DEFS_H
 
 #include "types.h"
-
+/*
+    e.g. uint64 sstatus_value = csr_read(sstatus);
+*/
 #define csr_read(csr)                       \
 ({                                          \
-    register uint64 __v;                    \
-    /* unimplemented */                     \
+    uint64 __v;                             \
+    asm volatile ("csrr %0, " #csr      \
+                    : "=r" (__v)             \
+                    :                       \
+                    : "memory");            \
     __v;                                    \
 })
 
+/*
+    csr_write macro does not return any value
+    e.g. csr_write(sscratch, 0);
+*/
 #define csr_write(csr, val)                         \
 ({                                                  \
     uint64 __v = (uint64)(val);                     \
