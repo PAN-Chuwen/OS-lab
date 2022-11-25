2f7eba159f0b85262bf1daf3c73f62a783f1674f
Pancw2000 <panchuwen2000@gmail.com>
2022-11-25 09:06:56 +0800

    Build lab2

--- src/lab2/init/main.c
+++ src/lab2/init/main.c
@@ -1,11 +1,9 @@
-#include "print.h"
+#include "printk.h"
 #include "sbi.h"
 
 extern void test();
 
 int start_kernel() {
-    puti(2022);
-    puts(" Hello RISC-V\n");
     test(); // DO NOT DELETE !!!
 
 	return 0;
--- src/lab2/init/test.c
+++ src/lab2/init/test.c
@@ -1,12 +1,11 @@
-#include "print.h"
+#include "printk.h"
 #include "defs.h"
 
 // Please do not modify
 
 void test() {
-    puts("write 0x0 to sscratch using csr_write\n");
+
     csr_write(sscratch, 0x13);
-    puts("read sstatus using csr_read\n");
     uint64 sstatus_value = csr_read(sstatus);
     
     while (1);
