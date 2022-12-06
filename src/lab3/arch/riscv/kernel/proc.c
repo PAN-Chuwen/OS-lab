0d610c94678ffdc7152e92c1b06975fe6205d1b3
PAN-Chuwen <panchuwen2000@gmail.com>
2022-12-06 16:33:59 +0800

    Add task_init

--- src/lab3/arch/riscv/kernel/proc.c
+++ src/lab3/arch/riscv/kernel/proc.c
@@ -1,5 +1,8 @@
 #include "proc.h"
 #include "printk.h"
+#include "mm.h"
+#include "rand.h"
+#include "defs.h"
 
 extern void __dummy();
 
@@ -9,19 +12,37 @@ struct task_struct* task[NR_TASKS]; // 线程数组, 所有的线程都保存在
 
 void task_init() {
     // 1. 调用 kalloc() 为 idle 分配一个物理页
+    idle = (struct task_struct*) kalloc();
     // 2. 设置 state 为 TASK_RUNNING;
+    idle->state = TASK_RUNNING;
     // 3. 由于 idle 不参与调度 可以将其 counter / priority 设置为 0
+    idle->counter = 0;
+    idle->priority = 0;
     // 4. 设置 idle 的 pid 为 0
+    idle->pid = 0;
     // 5. 将 current 和 task[0] 指向 idle
+    current = idle;
+    task[0] = idle;
+    // note that struct member thread of idle should not be used
 
-    /* YOUR CODE HERE */
-
+    uint64 currentPID;
+    for (currentPID = 1; currentPID < NR_TASKS; currentPID++) {
     // 1. 参考 idle 的设置, 为 task[1] ~ task[NR_TASKS - 1] 进行初始化
+        task[currentPID] = (struct task_struct*) kalloc();
+
     // 2. 其中每个线程的 state 为 TASK_RUNNING, counter 为 0, priority 使用 rand() 来设置, pid 为该线程在线程数组中的下标。
+        task[currentPID]->state = TASK_RUNNING;
+        task[currentPID]->counter = 0;
+        task[currentPID]->priority = rand();
+        task[currentPID]->pid = currentPID;
+
     // 3. 为 task[1] ~ task[NR_TASKS - 1] 设置 `thread_struct` 中的 `ra` 和 `sp`,
     // 4. 其中 `ra` 设置为 __dummy （见 4.3.2）的地址,  `sp` 设置为 该线程申请的物理页的高地址
+        task[currentPID]->thread.ra = (uint64) __dummy;
+        task[currentPID]->thread.sp = (uint64)task[currentPID] + PGSIZE;
+    }
+   
 
-    /* YOUR CODE HERE */
 
     printk("...proc_init done!\n");
 }
